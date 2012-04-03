f1a4be4f7171353448fd871d9bd1815117916e7b
RockingChair LLC <rockingchairllc@gmail.com>
2012-04-03 15:14:55 -0400

    committed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20120331232621) do
 
   create_table "answers", :force => true do |t|
