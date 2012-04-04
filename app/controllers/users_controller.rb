31bd61c6b6224781c2e4d3131bb3ce5f14236f3a
RockingChair LLC <rockingchairllc@gmail.com>
2012-04-04 15:42:09 -0400

    added some shit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < Devise::RegistrationsController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @questions = Question.all
 
     respond_to do |format|
       format.html # show.html.erb
