b94cacf1bb4dbbf9b8658d504883c7b4dec7514d
Matt Pestritto <matt@pestritto.com>
2012-04-13 18:18:21 -0400

    watchlist pulling real content on movie page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,7 @@
 class ProfileController < ApplicationController
   before_filter :authenticate_user!
   def profile
+    @user=User.find(current_user.id)
+    @watch_list=@user.watch_lists
   end
 end
