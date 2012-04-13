ea833be03adb11bc99df6edea6a62a979304fad9
Matt Pestritto <matt@pestritto.com>
2012-04-13 18:33:56 -0400

    optimize query - include

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,6 @@ class ProfileController < ApplicationController
   before_filter :authenticate_user!
   def profile
     @user=User.find(current_user.id)
-    @watch_list=@user.watch_lists
+    @watch_list=@user.watch_lists.all(:include=>[:movie])
   end
 end
