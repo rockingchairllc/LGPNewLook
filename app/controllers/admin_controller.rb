4c046fa59bc745eb69316caaafbe4b6a2b065fc9
Matt Pestritto <matt@themutual.com>
2012-04-14 18:53:46 -0400

    added auth for admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,11 @@
 class AdminController < ActionController::Base
+  before_filter :authorize_admin
 
+  def authorize_admin
+    if current_user && current_user.is_admin
+      true
+    else
+      false
+    end
+  end
 end
