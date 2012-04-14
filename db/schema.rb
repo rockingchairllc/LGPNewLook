fff0b625d200ed05d5a2d6d4246146fbd9b21973
Matt Pestritto <matt@themutual.com>
2012-04-14 18:35:47 -0400

    migration for is_admin added to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120404230024) do
+ActiveRecord::Schema.define(:version => 20120414223417) do
 
   create_table "answers", :force => true do |t|
     t.text     "response"
@@ -96,12 +96,12 @@ ActiveRecord::Schema.define(:version => 20120404230024) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "firstname",                              :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "gender",                                 :null => false
-    t.string   "orientation",                            :null => false
-    t.integer  "zipcode",                                :null => false
+    t.string   "firstname",                                 :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "gender",                                    :null => false
+    t.string   "orientation",                               :null => false
+    t.integer  "zipcode",                                   :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -110,9 +110,10 @@ ActiveRecord::Schema.define(:version => 20120404230024) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.datetime "birthdate"
+    t.boolean  "is_admin",               :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
