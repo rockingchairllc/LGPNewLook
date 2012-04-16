a6daadb937382be70a556687c8d5f6a79b9ec2a4
RockingChair LLC <rockingchairllc@gmail.com>
2012-04-15 20:35:49 -0400

    user profile working but have to test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < Devise::RegistrationsController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @watch_list = @user.watch_lists
     @questions = Question.active
 
     respond_to do |format|
