560f3d060e6529a5956693a8fa8c436a11c0102a
Matt Pestritto <matt@pestritto.com>
2012-04-17 17:03:18 -0400

    added modal popup on success invite code

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
   $('#form_invite_code').bind('ajax:success', function(evt, data, status, xhr) { generic_form_handler(data, invite_success ); });
 });
 
-function invite_success(){
-  alert('hi');
+function show_modal(id){
+  $('#' + id).modal({
+     show : true,
+     keyboard : true,
+     backdrop : true
+  });
 }
+
+function invite_success(){
+  show_modal('modal_sign_up');
+};
