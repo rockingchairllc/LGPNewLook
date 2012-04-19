92a6c8eb76fbbbc5fbabfe7f0c4f446f545c112d
qen <qen.empaces@gmail.com>
2012-04-20 00:05:10 +0800

    ajax fixes

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -12,7 +12,6 @@
     var movie_id = self.attr('data-movie-id');
 
     self.find('a.remove').click(function(){
-      self.removeClass('is_watched');
       console.log('remove here');
       
       $.post('/watch_lists/0', {'_method' : 'delete', 'user_id' : user_id, 'movie_id' : movie_id },
@@ -20,23 +19,22 @@
         console.log(data);
         if (data.success){
           // do something to show it was added.
-          set_not_watching_movie(movie_id);
+          self.removeClass('is_watched');
         } else{
           // handle the error however you want.... display error returned from app or generic message.
-        };
+        }
       }, 'json' );
 
     });
     
     self.find('a.add').click(function(){
-      self.addClass('is_watched');
 
       $.post('/watch_lists', {'user_id' : user_id, 'movie_id' : movie_id },
         function(data){
           console.log(data);
           if (data.success){
             // do something to show it was added.
-            set_watching_movie(movie_id);
+            self.addClass('is_watched');
           } else{
             // handle the error however you want.... display error returned from app or generic message.
           }
