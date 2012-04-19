d7fd61d0322e9345bd83bf59c940e5cf2200ccbc
qen <qen.empaces@gmail.com>
2012-04-20 00:00:00 +0800

    movies layout working

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -1,6 +1,48 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+(function($){
+  
+  $('#zip_code_filter').click(function(){
+    var miles = $('#select_miles').val();
+    var zip   = $('#field_zip').val();
+    window.location='/movies?miles=' + miles + '&zip=' + zip;
+  });
 
+  $('.watchlist-action').each(function(){
+    var self = $(this);
+    var user_id = 1;
+    var movie_id = self.attr('data-movie-id');
 
+    self.find('a.remove').click(function(){
+      self.removeClass('is_watched');
+      console.log('remove here');
+      
+      $.post('/watchlist/0', {'_method' : 'delete', 'user_id' : user_id, 'movie_id' : movie_id },
+      function(data){
+        console.log(data);
+        if (data.success){
+          // do something to show it was added.
+          set_not_watching_movie(movie_id);
+        } else{
+          // handle the error however you want.... display error returned from app or generic message.
+        };
+      }, 'json' );
+
+    });
+    
+    self.find('a.add').click(function(){
+      self.addClass('is_watched');
+
+      $.post('/watchlist', {'user_id' : user_id, 'movie_id' : movie_id },
+        function(data){
+          console.log(data);
+          if (data.success){
+            // do something to show it was added.
+            set_watching_movie(movie_id);
+          } else{
+            // handle the error however you want.... display error returned from app or generic message.
+          }
+      }, 'json' );
+      
+    });
+  })
+
+})(jQuery);
