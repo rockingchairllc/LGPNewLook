7e8374e8fbb3f1ad8e009b02a72dc456c66293eb
RockingChair LLC <rockingchairllc@gmail.com>
2012-04-22 23:01:07 -0400

    movies views

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ActiveRecord::Base
   has_many :schedules, :inverse_of => :movie
   has_many :theaters, :through => :schedules, :uniq => true
   has_many :watch_lists
+  has_many :watchlisters, :source => :user, :through => :watch_lists
   has_attached_file :poster, :storage => :s3, :s3_credentials => "#{Rails.root}/config/s3.yml", :path => "movieposters/:id/:filename"
 
 
