399d2106f3f278c7aef68a685c3853ceec92c334
qen <qen.empaces@gmail.com>
2012-04-24 09:34:42 +0800

    move ellipses into shorten helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ def shorten (string, count = 210)
 		shortened = string[0, count]
 		splitted = shortened.split(/\s/)
 		words = splitted.length
-		splitted[0, words-1].join(' ')
+		(splitted[0, words-1].join(' ') + '&hellip;').html_safe
 	else
 		string
 	end
