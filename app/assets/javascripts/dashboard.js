551579a69e497f0a60ff40aa55f9e91c6a68a829
Matt Pestritto <matt@pestritto.com>
2012-04-30 17:55:50 -0400

    moved js to js files from html

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,6 +1,9 @@
 
 $(document).ready(function () {
-  $('#form_user_profile').bind('ajax:success', function(evt, data, status, xhr) { generic_form_handler(data, profile_update_success, 'form_user_profile_errors'); });
+  if ($('#modal_user_profile_exists').attr('exists') == "1"){
+    show_modal('modal_user_profile');
+    $('#form_user_profile').bind('ajax:success', function(evt, data, status, xhr) { generic_form_handler(data, profile_update_success, 'form_user_profile_errors'); });
+  }
 });
 
 function submit_profile_form(){
