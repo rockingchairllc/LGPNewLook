158a3b7a05993ecadf65ee58bf132528777f9c66
RockingChair LLC <rockingchairllc@gmail.com>
2012-04-30 18:07:42 -0400

    deleted files

--- app/controllers/profile_controller.rb
+++
@@ -1,7 +0,0 @@
-class ProfileController < ApplicationController
-  before_filter :authenticate_user!
-  def profile
-    @user=User.find(current_user.id)
-    @watch_list=@user.watch_lists.all(:include=>[:movie])
-  end
-end
