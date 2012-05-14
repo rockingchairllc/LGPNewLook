8e8b359c98e21f7e5a5151eb885d34fe336113e0
Sebastian Glazebrook <me@sebglazebrook.com>
2012-05-14 11:59:27 +1000

    disabled email notifications for mailboxer messages

--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -1,13 +1,13 @@
 Mailboxer.setup do |config|
   
   #Configures if you applications uses or no the email sending for Notifications and Messages
-  config.uses_emails = true
+  config.uses_emails = false
   
   #Configures the default from for the email sent for Messages and Notifications of Mailboxer
-  config.default_from = "no-reply@mailboxer.com"
+  config.default_from = "no-reply@mailboxer.com"    # replace this if we start doing email notifications
   
   #Configures the methods needed by mailboxer
-  config.email_method = :mailboxer_email
+  config.email_method = :mailboxer_email    #add this field to user if we start doing email notifications
   config.name_method = :name
 
   #Configures if you use or not a search engine and wich one are you using
